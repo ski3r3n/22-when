83692dc944ae6e600cb585261fba55c9e3a90576
Kie Ren <kieren.siew@gmail.com>
2023-07-05 14:12:00 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-i = 0
-
 def on_gesture_shake():
     global i
     i += 1
@@ -7,6 +5,13 @@ def on_gesture_shake():
         i = 0
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+i = 0
+i = 0
+
 def on_forever():
     basic.show_number(i)
 basic.forever(on_forever)
+
+def on_forever2():
+    pass
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,14 +1,27 @@
-input.onGesture(Gesture.Shake, function () {
+input.onButtonPressed(Button.A, function () {
+    onInput()
+})
+function onInput () {
     i += 1
     if (i == 10) {
         i = 0
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            . . . . .
+            # . # . #
+            `)
     }
+}
+input.onButtonPressed(Button.B, function () {
+    onInput()
+})
+input.onGesture(Gesture.Shake, function () {
+    onInput()
 })
 let i = 0
 i = 0
 basic.forever(function () {
     basic.showNumber(i)
-})
-basic.forever(function () {
-	
 })
